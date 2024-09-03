3f004f59810e2e2e83e1238fa6d405bb02ba8a3b
zhangxiaoxu <31872163@qq.com>
2024-09-03 20:41:00 +0800

    fix:json format

--- array/funcs_json.go
+++ array/funcs_json.go
@@ -157,9 +157,9 @@ func encodeZval(val api.IZVal, writer io.Writer, optInfo *api.JsonOptInfo) (err 
 	switch val.Kind() {
 	case kind.String, kind.Bytes:
 		v := val.String()
+		v = strings.ReplaceAll(v, "\\", "\\\\")
 		v = strings.ReplaceAll(v, "\"", "\\\"")
 		v = strings.ReplaceAll(v, "\n", "\\n")
-		v = strings.ReplaceAll(v, "\\", "\\\\")
 		if _, err = writer.Write([]byte{byte('"')}); err == nil {
 			if _, err = writer.Write([]byte(v)); err == nil {
 				_, err = writer.Write([]byte{byte('"')})
